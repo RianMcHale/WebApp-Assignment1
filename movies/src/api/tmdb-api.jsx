69e5387110df5c2306bfd37e68f1a7f2c7c43a86
RianMcHale <rianmchwork@gmail.com>
2025-11-02 15:58:24 +0000

    Added new API functions for new pages.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -15,7 +15,6 @@ export const getMovies = () => {
 };
 
 
-
 export const getMovie = (args) => {
   //console.log(args)
   const [, idPart] = args.queryKey;
@@ -99,5 +98,54 @@ export const getUpcomingMovies = () => {
     .then((json) => json.results);
 };
 
+export const getPopularMovies = (page = 1) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+  )
+    .then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+};
 
+export const getTopRatedMovies = (page = 1) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const getTrendingMovies = (mediaType = 'movie', timeWindow = 'day', page = 1) => {
+  return fetch(
+    `https://api.themoviedb.org/3/trending/${mediaType}/${timeWindow}?api_key=${import.meta.env.VITE_TMDB_KEY}&page=${page}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 
