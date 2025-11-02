8e3f8a3ec3a58ef0e6634ae48c1fd49e9bc330c1
RianMcHale <rianmchwork@gmail.com>
2025-11-02 15:36:11 +0000

    Created popular movies page.

---
+++ movies/src/pages/popularMoviesPage.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getPopularMovies } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
+import Spinner from "../components/spinner";
+import Pagination from "@mui/material/Pagination";
+import Grid from "@mui/material/Grid";
+
+const PopularMoviesPage = () => {
+    const [page, setPage] = useState(1);
+
+    const { data, error, isPending, isError } = useQuery ({
+        queryKey: ["popular", { page }],
+        queryFn: () => getPopularMovies(page),
+        keepPreviousData: true,
+    });
+
+    if (isPending) {
+        return <Spinner />
+    }
+    if (isError) {
+        return <h1>{error.message}</h1>
+    }
+    const movies = data.results;
+    const totalPages = Math.min(data.total_pages, 500);
+
+    return (
+        <>
+          <PageTemplate
+          title="Popular Movies"
+          movies={movies}
+          action={(movie) => <AddToPlaylistIcon movie={movie} />}
+          />
+          <Grid container justifyContent="center" sx={{ paddingBottom: 4}}>
+            <Pagination
+            count={totalPages}
+            page={page}
+            color="primary"
+            onChange={(event, value) => setPage(value)}
+            />
+          </Grid>
+    </>
+    );
+};
+
+export default PopularMoviesPage;
